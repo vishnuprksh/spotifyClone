4496883fd65f25a0b34cd0c7bb0ee03cf11efb6a
vishnuprksh <vishnucheppanam@gmail.com>
2023-06-07 13:10:23 +0530

    added click options

--- script.js
+++ script.js
@@ -41,3 +41,34 @@ function toggleLanguageWindow() {
 
 // Attach click event listener to the language button
 languageButton.addEventListener('click', toggleLanguageWindow);
+
+
+// for toggling the menu bar
+function toggleSidebar() {
+    var sidebar = document.querySelector('.sidebar');
+    var mainContent = document.querySelector('main');
+
+    if (sidebar.style.display === 'none') {
+        sidebar.style.display = 'block';
+    } else {
+        sidebar.style.display = 'none';
+    }
+}
+
+window.addEventListener('resize', function () {
+    var sidebar = document.querySelector('.sidebar');
+    var mainContent = document.querySelector('main');
+
+    if (window.innerWidth > 500) {
+        sidebar.style.display = 'block';
+        mainContent.style.marginLeft = '200px'; // Adjust the margin when the viewport is wider than 500px
+    } else {
+        sidebar.style.display = 'none';
+        mainContent.style.marginLeft = '0'; // Reset the margin when the viewport is narrower than 500px
+    }
+});
+
+
+// Attach click event listener to the menu button
+const menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', toggleSidebar);
